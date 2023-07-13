cd3549873427c9b151f0d32fb0ea3de98d8faf1a
WandaSimeoli <wandasimeoli@gmail.com>
2023-07-13 18:22:57 +0200

    Contenuto Header

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,34 @@ export default {
 </script>
 
 <template>
- <header></header>
- <main></main>
- <footer></footer>
+ <header class="debug">
+  <div class="row debug ">
+    <div class="col debug">
+      <img src="./assets/img/dc-logo.png" alt="">
+    </div>
+    <div class="col debug">
+      <nav class="debug">
+        <ul class="debug"> <a href=""><li class="debug">Characters</li></a>
+        <a href=""><li class="debug">Comics</li></a>
+        <a href=""><li class="debug">Movies</li></a>
+        <a href=""><li class="debug">TV</li></a>
+        <a href=""><li class="debug">Games</li></a>
+        <a href=""><li class="debug">Collectibles</li></a>
+        <a href=""><li class="debug">Videos</li></a>
+        <a href=""><li class="debug">Fans</li></a>
+        <a href=""><li class="debug">news</li></a>
+        <a href=""><li class="debug">shop</li></a>
+       </ul>
+      </nav>
+    </div>
+  </div>
+ </header>
+ <main class="debug">
+  <section></section>
+ </main>
+ <footer class="debug"></footer>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @use "assets/scss/main.scss";
 </style>
